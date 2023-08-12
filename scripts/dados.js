c221633265a76b47c42a9b8d110681cbdba3c52b
Gustavo <gmalmeida26@gmail.com>
2023-08-12 14:11:43 -0300

    Ex 1 - [Dados] Criando um JSON com os dados

---
+++ scripts/dados.js
@@ -0,0 +1,25 @@
+// Ex.1) Crie um arquivo Javascript chamado de dados.js para armazenar os dados dos usuários. Nele deverá conter um array de usuarios. no formato JSON. Cada usuário deve ter:
+
+// email
+// nome
+// senha
+
+let dadosUsuarios = `
+    [
+        {
+            "nome": "Gustavo",
+            "email": "gustavo@email.com.br",
+            "senha": 123456
+        },
+        {
+            "nome": "Joao",
+            "email": "joao@email.com.br",
+            "senha": 123456
+        },
+        {
+            "nome": "Maria",
+            "email": "maria@email.com.br",
+            "senha": 123456
+        }
+    ]
+`
